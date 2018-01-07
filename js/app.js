af70279e108f1bc9e1af3a97f46329061764f1b6
franciscohanna92 <franciscohanna92@gmail.com>
2018-01-07 16:45:53 -0300

    Pequeño cambio en el script principal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log("Hola mundo!");
 
-const  map = new google.maps.Map(document.getElementById("map"), {
+const map = new google.maps.Map(document.getElementById("map"), {
   center: { lat: -34.397, lng: 150.644 },
   zoom: 8
 });
