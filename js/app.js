83943b4e7aab23f0ca5a3853deb96c7da2291f84
Facundo Villard <facuvillard@gmail.com>
2018-02-03 18:04:36 -0300

    Se agrego los marcadores de los sismos en el mapa.

--- js/app.js
+++ js/app.js
@@ -5,39 +5,34 @@ const map = new google.maps.Map(document.getElementById("map"), {
   zoom: 8
 });
 
-
 const config = {
   apiKey: "AIzaSyD7PRyMw8tX_YN1F5KIPAhMr7og0ha27YQ",
   authDomain: "inpres-scrapper.firebaseapp.com",
   databaseURL: "https://inpres-scrapper.firebaseio.com/",
 };
 firebase.initializeApp(config);
-
 var sismos = [];
 var database = firebase.database();
 
 /**
  * Listener para cambios en base de datos
  */
+
 var Sismos = firebase.database().ref('Sismos').orderByKey();
 Sismos.on('value', function(snapshot) {
   // console.log(snapshot.val());
   snapshot.forEach(function(childSnapshot) {
     // console.log(childSnapshot.val().timestamp)
-    sismos.unshift(childSnapshot)
+    sismos.unshift(childSnapshot.val())
   });
-
-  // Limpiamos lista y redibujamos
-  $('#lista-sismos').html('');
-  dibujarListaSismos();
-
-  // Agregamos click listener para elementos de lista
-  $('.sismos-list-item').click(sismoClickListener);
+  
 });
 
 /**
  * Dibujar lista HTML de sismos en base de datos
  */
+
+ 
 function dibujarListaSismos() {
   for (let i = 0; i < sismos.length; i++) {
     let sismo = sismos[i].val();
@@ -50,31 +45,32 @@ function dibujarListaSismos() {
   }
 }
 
-function sismoClickListener() {
-  let sismoTimestamp = this.id;
-  let sismo;
-  for (let i = 0; i < sismos.length; i++) {
-    if(sismos[i].val().timestamp == sismoTimestamp) {
-      sismo = sismos[i].val();
-      break;
+var lista_sismos = new Vue({
+  el: '#lista-sismos',
+  data: {
+    listaSismos: sismos
+  },
+
+  methods: { 
+    marcarEnMapa: function(latitud, longitud) {
+      let latLong = new google.maps.LatLng(latitud, longitud)
+      let marker = new google.maps.Marker({
+        animation: google.maps.Animation.DROP,
+        position: latLong,
+        map: map
+      })
+      map.setCenter(latLong)
+
     }
   }
-  // con  sole.log(sismo);
+});
 
-  // Dibujamos marcador
-  let markerPos = new google.maps.LatLng(-25.363, 131.044);
-  var marker = new google.maps.Marker({
-    position: markerPos,
-    map: map
-  });
+/**
 
-  map.setCenter(markerPos);
-};
+var google_maps = new Vue({
+  el: '#lista-sismos',
+  
+  }
+})
 
-
-
-
-
-
-
-
+ */
